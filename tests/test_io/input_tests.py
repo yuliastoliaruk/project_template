56282d0a449dd0951253006d36ec9ef04f7ca5ce
yuliiastoliaruk <uliastolaruk@gmail.com>
2024-03-18 23:50:59 +0200

    Add tests to test function read_file_pandas from app/io/input.py

--- tests/test_io/input_tests.py
+++ tests/test_io/input_tests.py
@@ -1,5 +1,6 @@
 import pytest
 import app.io.input as input
+import pandas as pd
 
 
 def test_read_file():
@@ -12,3 +13,15 @@ def test_read_file():
 def test_read_file_error():
     with pytest.raises(FileNotFoundError):
         input.read_file('no_file')
+
+
+def test_read_file_pandas():
+    results = input.read_file_pandas('data/input.txt')
+    file = pd.read_csv('data/input.txt')
+    data = ' '.join(file.iloc[:, 0].astype(str).tolist())
+    assert results == data
+
+
+def test_read_file_pandas_error():
+    with pytest.raises(FileNotFoundError):
+        input.read_file_pandas('no_file')
